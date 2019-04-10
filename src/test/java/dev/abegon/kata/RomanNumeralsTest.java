713999d3e206f8b37daf170d103aacdfa69dc6b6
Marc Abegon <marc.abegon@gmail.com>
2019-04-10 23:55:47 +0200

    Refactor unit tests to parametrized tests

--- src/test/java/dev/abegon/kata/RomanNumeralsTest.java
+++ src/test/java/dev/abegon/kata/RomanNumeralsTest.java
@@ -1,96 +1,46 @@
 package dev.abegon.kata;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(value = Parameterized.class)
 public class RomanNumeralsTest {
 
-    @Test
-    public void Should_return_I_when_input_is_1()
-    {
-        assertEquals("I", RomanNumerals.arabicToRoman(1));
+    @Parameterized.Parameter(value = 0)
+    public int arabicNumber;
+
+    @Parameterized.Parameter (value = 1)
+    public String romanNumber;
+
+    @Parameterized.Parameters(name = "{index}: Should_return_{1}_when_input_is_{0}()")
+    public static Collection data() {
+        return Arrays.asList(new Object[][]{
+                {1, "I"},
+                {2, "II"},
+                {3, "III"},
+                {4, "IV"},
+                {5, "V"},
+                {6, "VI"},
+                {9,"IX"},
+                {14,"XIV"},
+                {40,"XL"},
+                {50,"L"},
+                {90,"XC"},
+                {100,"C"},
+                {1000,"M"},
+                {3000,"MMM"}
+        });
     }
 
     @Test
-    public void Should_return_II_when_input_is_2()
+    public void test_arabicToRoman()
     {
-        assertEquals("II", RomanNumerals.arabicToRoman(2));
+        assertEquals(romanNumber, RomanNumerals.arabicToRoman(arabicNumber));
     }
-
-    @Test
-    public void Should_return_III_when_input_is_3()
-    {
-        assertEquals("III", RomanNumerals.arabicToRoman(3));
-    }
-
-
-    @Test
-    public void Should_return_IV_when_input_is_4()
-    {
-        assertEquals("IV", RomanNumerals.arabicToRoman(4));
-    }
-
-    @Test
-    public void Should_return_V_when_input_is_5()
-    {
-        assertEquals("V", RomanNumerals.arabicToRoman(5));
-    }
-
-    @Test
-    public void Should_return_VI_when_input_is_6()
-    {
-        assertEquals("VI", RomanNumerals.arabicToRoman(6));
-    }
-
-
-    @Test
-    public void Should_return_IX_when_input_is_9()
-    {
-        assertEquals("IX", RomanNumerals.arabicToRoman(9));
-    }
-
-    @Test
-    public void Should_return_XIV_when_input_is_14()
-    {
-        assertEquals("XIV", RomanNumerals.arabicToRoman(14));
-    }
-
-    @Test
-    public void Should_return_XL_when_input_is_40()
-    {
-        assertEquals("XL", RomanNumerals.arabicToRoman(40));
-    }
-
-    @Test
-    public void Should_return_L_when_input_is_50()
-    {
-    assertEquals("L", RomanNumerals.arabicToRoman(50));
-    }
-
-    @Test
-    public void Should_return_XC_when_input_is_90()
-    {
-    assertEquals("XC", RomanNumerals.arabicToRoman(90));
-    }
-
-    @Test
-    public void Should_return_C_when_input_is_100()
-    {
-    assertEquals("C", RomanNumerals.arabicToRoman(100));
-    }
-
-    @Test
-    public void Should_return_M_when_input_is_1000()
-    {
-    assertEquals("M", RomanNumerals.arabicToRoman(1000));
-    }
-
-    @Test
-    public void Should_return_MMM_when_input_is_3000()
-    {
-        assertEquals("MMM", RomanNumerals.arabicToRoman(3000));
-    }
-
 }
